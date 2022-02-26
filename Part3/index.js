8e12e5bc87225c1ea274601b2bc11af716274001
benNjogu <kamandemachafi.gmail.com>
2022-02-26 21:36:04 +0300

    Arithmetic operators

--- Part3/index.js
+++ Part3/index.js
@@ -1,9 +1,18 @@
-let name = 'Ben';
-console.log(name);
 
-//<Rules for naming a variable>
-//They cannot be a reserved keyword
-//They should be meaningful
-//They cannot start with a number (1name)
-//They cannot contain a space or a hyphen(-)
-//Are case sensitive (firstName and FirstName are different)
+let x = 10;
+let y = 3;
+
+console.log(x + y);//x + y is an expression(something that produces a value)
+console.log(x - y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);//exponentiation
+
+//increment(++)
+console.log(++x);
+console.log(x++);
+console.log(x);
+//decrement(--)
+console.log(--x);
+console.log(x--);
+console.log(x);
