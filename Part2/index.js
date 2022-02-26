aec5e447f2ff22a6f3b65c3b20a36895ae4eb116
benNjogu <kamandemachafi.gmail.com>
2022-02-26 18:22:24 +0300

    array literal

--- Part2/index.js
+++ Part2/index.js
@@ -1,12 +1,2 @@
-//Reference types
-let person = {
-    name: 'Mosh',
-    age: 20
-}
-//Dot notation
-person.name = 'kimangas';
-
-//Bracket notation
-person['name'] = 'mary';
-
-console.log(person.name); 
+//Array literal
+let selectedColors = [];
