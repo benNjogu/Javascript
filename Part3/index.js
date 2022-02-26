f70cb7dfbd59beac9b51277d0408c27d83d21a02
benNjogu <kamandemachafi.gmail.com>
2022-02-26 19:46:00 +0300

    Variables

---
+++ Part3/index.js
@@ -0,0 +1,9 @@
+let name = 'Ben';
+console.log(name);
+
+//<Rules for naming a variable>
+//They cannot be a reserved keyword
+//They should be meaningful
+//They cannot start with a number (1name)
+//They cannot contain a space or a hyphen(-)
+//Are case sensitive (firstName and FirstName are different)
