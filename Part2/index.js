40388eb1c7855b58cb7c1dde410ebfb63ff35efe
benNjogu <kamandemachafi.gmail.com>
2022-02-26 17:38:41 +0300

    Primitive data types

--- Part2/index.js
+++ Part2/index.js
@@ -1,3 +1,7 @@
-//with constants the value can not change. 
-const interestRate = 0.3;
-console.log(interestRate);
+//Primitive variables
+let name = 'Ben';//String Literal
+let age = 20;//Number Literal
+let isApproved = true;//Boolean Literal
+let firstName = undefined;
+let lastName = null;
+let selectedColor = 'red';
