bc73d5932507b705b0fa7258ce52add6e7655f60
benNjogu <kamandemachafi.gmail.com>
2022-02-26 19:08:03 +0300

    parameters and arguments

--- Part2/index.js
+++ Part2/index.js
@@ -1,6 +1,9 @@
 //Functions
-function greet(){
-    console.log('hello world')
+function greet(name, lastName) {
+    //name here is a parameter
+    console.log('hello ' + name + ' ' + lastName);
 }
 
-greet();
+greet('Ben');//ben here is an argument
+greet('dianah');
+//lastName above by default is undefined
