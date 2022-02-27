8ea62f1ae0bff27db8367613fc04db36ebd748e2
benNjogu <kamandemachafi.gmail.com>
2022-02-27 07:21:14 +0300

    continue

--- Part4/index.js
+++ Part4/index.js
@@ -2,8 +2,16 @@
 let i = 0;
 
 while(i <= 10){
-    if(i === 5) break;
+    if(i % 2 === 0){
+        i++;
+        continue;
+    } 
     console.log(i);
     i++;
 }
-  //prints 0-4  
+  //prints odd numbers between 0 and 10
+  
+  /*
+  with break statement we jump out of the loop
+  with continue we jump to the next iteration
+  */
