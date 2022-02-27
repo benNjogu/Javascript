049346ae5268525c1d811cbf24060f2d1ce9ea4e
benNjogu <kamandemachafi.gmail.com>
2022-02-27 04:52:12 +0300

    Bitwise operators

--- Part3/index.js
+++ Part3/index.js
@@ -1,24 +1,26 @@
 
-//<Falsy>
-//undefined 
-//null
-//0
-//false
-//''
-//NaN
+//1 = 00000001
+//2 = 00000010
 
-//<Truthy>
-//anything that is not falsy
+//3 = 00000011
+//0 = 00000000
 
-console.log(false || true);
-console.log(false || 'Ben');
-console.log(false ||  1);
-//short-circuiting
-console.log(false || 1 || 2);
+console.log(1 | 2);//3
+console.log(1 & 2);//0
+console.log('------------------')
 
-console.log("--------------------");
+//Read, write and execute
+//00000100 ->for read permission only
+//00000010 ->for write permissions
+//00000001 ->for execute
 
-let userColor = 'red';
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
-console.log(currentColor);
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission;
+console.log(myPermission);
+
+let message = (myPermission & readPermission) ? 'Yes' : 'No';
+console.log(message);
