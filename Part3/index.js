ded4ebadc0427002d14a6fe002faa60cf2f4b7ee
benNjogu <kamandemachafi.gmail.com>
2022-02-27 05:01:04 +0300

    Operators precedence

--- Part3/index.js
+++ Part3/index.js
@@ -1,26 +1,7 @@
 
-//1 = 00000001
-//2 = 00000010
+let x = 2 + 3 * 4;
+console.log(x);//=14
 
-//3 = 00000011
-//0 = 00000000
-
-console.log(1 | 2);//3
-console.log(1 & 2);//0
-console.log('------------------')
-
-//Read, write and execute
-//00000100 ->for read permission only
-//00000010 ->for write permissions
-//00000001 ->for execute
-
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
-console.log(myPermission);
-
-let message = (myPermission & readPermission) ? 'Yes' : 'No';
-console.log(message);
+console.log('------------')
+let y = (2 + 3) * 4;
+console.log(y);//=20
