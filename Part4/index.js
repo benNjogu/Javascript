5943261c788d5daacc174ab043db4cd3df27c6fd
benNjogu <kamandemachafi.gmail.com>
2022-02-27 06:20:44 +0300

    while loop

--- Part4/index.js
+++ Part4/index.js
@@ -1,19 +1,10 @@
 
-//initialExpression, condition, incrementExpression
-for(let i = 0; i < 5; i++)
-    console.log('hello world', i);
+let i = 0;
 
-console.log('--------------');
-
-for(let i = 1; i <= 5; i++){
-    if(i%2 !== 0)
-        console.log(i);
+while(i <= 5){
+    if(i % 2 !== 0) console.log(i); 
+    i++;
 }
+    
 
-console.log('--------------');
-
-for(let i = 5; i >= 1; i--){
-    if(i%2 !== 0)
-        console.log(i);
-}
     
