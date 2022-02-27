7ab54f4e7a31c3fa454190e7858c01ccabc96efb
benNjogu <kamandemachafi.gmail.com>
2022-02-27 17:11:04 +0300

    Challenge 10

--- Part5: Challenges/index.js
+++ Part5: Challenges/index.js
@@ -1,42 +1,20 @@
-//Challenge 9
-//Grading system
-//Calculate average first
-//1-59 = F
-//60-69 = D
-//70-79 = C
-//80-89 = B
-//90-100 = A
+//Challenge 10
+//Show stars
+
 
 //MY ANSWER
-const marks = [80, 80, 90];
+showStars(15);
 
-console.log(calculateGrade(marks));
-
-function calculateGrade(marks) {
-
-    const average = calculateAverage(marks);
-
-    if (average >= 90)
-        return 'A';
-    if (average >= 80)
-        return 'B';
-    if (average >= 70)
-        return 'C';
-    if (average >= 60)
-        return 'D';
-
-    return 'F';
+function showStars(rows) {
+    for (let row = 1; row <= rows; row++) {
+        let pattern = '';
+        for (let i = 0; i < row; i++)
+            pattern += '*';
+        console.log(pattern);
+    }
 }
 
-function calculateAverage(marks) {
-    let average = 0;
-    for (let mark of marks)
-        average += mark;
 
-    return average / marks.length;
-}
-
-console.log('-------------------')
 //TUTORS ANSWER
 
 
