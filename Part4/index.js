c240ced68d92972512223244f92cf6980813e7ec
benNjogu <kamandemachafi.gmail.com>
2022-02-27 07:05:26 +0300

    for of loop

--- Part4/index.js
+++ Part4/index.js
@@ -3,11 +3,13 @@ const person = {
     name: 'Ben',
     age: 30
 };
-    
+   
+//for in loop is best choice to iterate over properties(keys)
 for(let key in person)
     console.log(key, person[key]);
 
+//for of loop best choice to iterate over values 
 const colors = ['red', 'green', 'blue'];
-for(let index in colors)
-    console.log(index, colors[index]);
+for(let color of colors)
+    console.log(color);
     
