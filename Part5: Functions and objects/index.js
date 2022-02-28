7eee7179cee69df8d6c85a8377978158b21483db
benNjogu <kamandemachafi.gmail.com>
2022-02-28 03:58:23 +0300

    Basics

--- Part5: Functions and objects/index.js
+++ Part5: Functions and objects/index.js
@@ -1,1 +1,22 @@
 
+//Object Oriented Programming
+let radius = 1;
+let x = 1;
+let y = 1;
+function draw();
+
+
+//Putting the above values in an object
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw')
+    }
+};
+
+circle.draw();
