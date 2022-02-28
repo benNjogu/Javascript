12afe96d4a3ad9f46d4c14646a14c75d801f7b0c
benNjogu <kamandemachafi.gmail.com>
2022-02-28 19:20:06 +0300

    Enumerating properties of an Object

--- Part5: Functions and objects/index.js
+++ Part5: Functions and objects/index.js
@@ -9,5 +9,16 @@ const circle = {
 for (let key in circle)
     console.log(key, circle[key]);
 
+console.log('----------------');
+
 for (let key of Object.keys(circle))
     console.log(key);
+
+console.log('----------------');
+
+for (let entry of Object.entries(circle))
+    console.log(entry);
+
+console.log('----------------');
+
+if ('radius' in circle) console.log('yes');
