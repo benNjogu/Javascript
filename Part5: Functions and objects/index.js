6351f0627a10e93d240cdceefb81cb9406647b3a
benNjogu <kamandemachafi.gmail.com>
2022-02-28 05:45:44 +0300

    Constructor property

--- Part5: Functions and objects/index.js
+++ Part5: Functions and objects/index.js
@@ -13,3 +13,13 @@ delete circle.draw;
 //it but we can change it by adding or removing properties.
 
 console.log(circle);
+
+//if we write
+let x = {}
+//js engine will translate this to
+//let x = new Object();
+
+//every object has a constructor property which references the function that was uses to create that object.
+new String(); //'', "", ``
+new Boolean(); //true, false
+new Number();//1, 2, 3, ...
