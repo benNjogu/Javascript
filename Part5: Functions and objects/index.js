d64dc205a2e6407ca50373ee917ddd7501ad2e69
benNjogu <kamandemachafi.gmail.com>
2022-02-28 20:59:18 +0300

    Cloning objects

--- Part5: Functions and objects/index.js
+++ Part5: Functions and objects/index.js
@@ -8,7 +8,23 @@ const circle = {
 
 const another = {};
 
+//The old way to clone
 for (let key in circle)
     another[key] = circle[key];
 
 console.log(another);
+
+console.log('----------------------');
+
+//Another way to clone
+//This can be used to combine multiple objects into a single object
+const another1 = Object.assign({
+    color: 'yellow'
+}, circle);
+console.log(another1);
+
+console.log('----------------------');
+
+//Yet another way
+const another2 = { ...circle };
+console.log(another2);
