5946403b4c77685b09bbde37ba05c578aa07d0f6
benNjogu <kamandemachafi.gmail.com>
2022-03-01 05:18:43 +0300

    About garbage collection

--- Part5: Functions and objects/index.js
+++ Part5: Functions and objects/index.js
@@ -1,30 +1,8 @@
 
-const circle = {
-    radius: 1,
-    draw() {
-        console.log('draw');
-    }
-};
+let circle = {}
+console.log(circle);
 
-const another = {};
-
-//The old way to clone
-for (let key in circle)
-    another[key] = circle[key];
-
-console.log(another);
-
-console.log('----------------------');
-
-//Another way to clone
-//This can be used to combine multiple objects into a single object
-const another1 = Object.assign({
-    color: 'yellow'
-}, circle);
-console.log(another1);
-
-console.log('----------------------');
-
-//Yet another way
-const another2 = { ...circle };
-console.log(another2);
+/*
+In js you dont have to worry about garbage collection 
+as it happens behind the scenes
+*/
