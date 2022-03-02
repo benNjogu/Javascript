bb106e386bcde301184a3610af92088069d50999
benNjogu <kamandemachafi.gmail.com>
2022-03-02 19:12:24 +0300

    Combining and slicing arrays

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,23 +1,15 @@
 
+const first = [1, 2, 3];
+const second = [4, 5, 6];
 
-let numbers = [1, 2, 3, 4];
+const combined = first.concat(second);
+console.log(combined);
 
-//Emptying an array
-//Soln 1
-numbers = [];
+console.log('----------------');
 
-//Soln 2
-numbers.length = 0;
-
-//Soln 3
-//Using the splice method
-numbers.splice(0, numbers.length);
-
-//Soln 4
-//pop
-while (numbers.length > 0)
-    numbers.pop();
-
-console.log(numbers);
-
-//Always go for soln 1 or 2(preferably)
+const sliced = combined.slice(2, 4);
+console.log(sliced);
+const sliced2 = combined.slice(2);
+console.log(sliced2);
+const sliced3 = combined.slice();//gives a copy of the original array
+console.log(sliced3);
