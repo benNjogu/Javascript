a887d9b233ea5017cc1f9a81edec76e038a07dae
benNjogu <kamandemachafi.gmail.com>
2022-03-02 16:58:54 +0300

    FInding elements

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,6 +1,13 @@
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 1, 4];
 
-let a = numbers.indexOf('a');
+console.log(numbers.indexOf('a'));//-1
+console.log(numbers.lastIndexOf(1));//3
+console.log('-------------------');
+console.log(numbers.indexOf(1));
+console.log(numbers.indexOf(1, 1));
+console.log('-------------------');
+//check if a certain number exists
+console.log(numbers.indexOf(1) === -1);
+console.log(numbers.includes(1));
 
-console.log(a);//-1
