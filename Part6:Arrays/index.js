dd2b98a84f728049c2181943581d3733c36820ff
benNjogu <kamandemachafi.gmail.com>
2022-03-02 19:19:27 +0300

    The spread operator

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,15 +1,19 @@
 
 const first = [1, 2, 3];
-const second = [4, 5, 6];
+const second = [4, 5, 6, 7];
 
-const combined = first.concat(second);
+//const combined = first.concat(second);
+const combined = [...first, 'a', ...second, 'b'];
 console.log(combined);
 
 console.log('----------------');
 
-const sliced = combined.slice(2, 4);
+/*const sliced = combined.slice(2, 4);
 console.log(sliced);
 const sliced2 = combined.slice(2);
 console.log(sliced2);
 const sliced3 = combined.slice();//gives a copy of the original array
-console.log(sliced3);
+console.log(sliced3);*/
+
+const copy = [...combined];
+console.log(copy);
