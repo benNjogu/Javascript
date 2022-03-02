18d7160d512bd2098111919517a58d4f108b5cae
benNjogu <kamandemachafi.gmail.com>
2022-03-02 16:40:10 +0300

    When element is not in the array

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,15 +1,6 @@
 
-const numbers = [3, 4];
+const numbers = [1, 2, 3, 4];
 
-//Adding elements at the end
-numbers.push(5, 6);
+let a = numbers.indexOf('a');
 
-
-//Adding elements at the beginning
-numbers.unshift(0, 1);
-
-
-//Adding elements at the middle
-numbers.splice(2, 0, 2);
-
-console.log(numbers);
+console.log(a);//-1
