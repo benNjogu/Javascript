a98781ca424c5896364f65e4888c1be2c0821f7b
benNjogu <kamandemachafi.gmail.com>
2022-03-02 16:36:55 +0300

    Adding elements

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,3 +1,15 @@
 
 const numbers = [3, 4];
-numbers = [];//Throws an error since number is a constant.
+
+//Adding elements at the end
+numbers.push(5, 6);
+
+
+//Adding elements at the beginning
+numbers.unshift(0, 1);
+
+
+//Adding elements at the middle
+numbers.splice(2, 0, 2);
+
+console.log(numbers);
