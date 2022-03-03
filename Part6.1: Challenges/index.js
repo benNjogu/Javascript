2a46143165924a1170f3d4d5b7cc8a3f4295d6c2
benNjogu <kamandemachafi.gmail.com>
2022-03-03 21:27:41 +0300

    Challenge 7: Movies, closes #4

--- Part6.1: Challenges/index.js
+++ Part6.1: Challenges/index.js
@@ -1,17 +1,22 @@
 /**
- * Challenge 6
- * Get max number in an array
+ * Challenge 7
+ * Get all the movies in 2018 with rating > 4
+ * Sort them by their rating 
+ * Descending order
+ * Pick their title
  */
 
-const numbers = [1, 2, 3, 3, 7];
+const movies = [
+    { title: 'a', year: 2022, rating: 4.5 },
+    { title: 'b', year: 2022, rating: 4.7 },
+    { title: 'c', year: 2022, rating: 3 },
+    { title: 'd', year: 2021, rating: 4.5 }
+];
 
-const max = getMax(numbers);
+const titles = movies
+    .filter(m => m.year === 2022 && m.rating >= 4)
+    .sort((a, b) => a.rating - b.rating)
+    .reverse()
+    .map(m => m.title);
 
-console.log(max);
-
-function getMax(array) {
-    if (array.lenght === 0) return undefined;
-
-    return array.reduce((a, b) => (a > b) ? a : b);
-}
-
+console.log(titles);
