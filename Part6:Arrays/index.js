c6e971e7f2f3c67d7f71751f7e7bde4d4993f41e
benNjogu <kamandemachafi.gmail.com>
2022-03-03 04:40:18 +0300

    Mapping to html. #6

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -3,4 +3,8 @@ const numbers = [1, -2, 3, 4];
 
 const filtered = numbers.filter(value => value >= 0);
 
-console.log(filtered);
+const items = filtered.map(n => '<li>' + n + '</li>')
+
+const html = '<ul>' + items.join('') + '</ul>';
+
+console.log(html);
