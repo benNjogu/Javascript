05cd3938e538af5a366818138f310c64d69a5ce4
benNjogu <kamandemachafi.gmail.com>
2022-03-03 05:22:52 +0300

    With accumulator initialized, #7.

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,9 +1,14 @@
 
-const numbers = [1, -2, 3, 4];
+const numbers = [1, 2, 3];
 
 //Soln 1
-let sum = 0;
+/*let sum = 0;
 for (let n of numbers)
-    sum += n;
+    sum += n;*/
+
+//Soln 2
+const sum = numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);//0 is the initial of the accumulator
 
 console.log(sum);
