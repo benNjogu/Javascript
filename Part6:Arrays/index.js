450ea6b44e754e5480ab3ca8618b051e9676d629
benNjogu <kamandemachafi.gmail.com>
2022-03-03 05:15:28 +0300

    Sum of an array, #7

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,14 +1,9 @@
 
 const numbers = [1, -2, 3, 4];
 
-const items = numbers
-    .filter(value => value >= 0)
-    .map(n => ({ value: n }))
-    .filter(obj => obj.value > 1)
-    .map(obj => obj.value);
-/**
- * @Use brackets () around an object when 
- * returning it using the arrow function.
- */
+//Soln 1
+let sum = 0;
+for (let n of numbers)
+    sum += n;
 
-console.log(items);
+console.log(sum);
