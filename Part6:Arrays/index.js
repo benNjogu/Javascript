a36cd030677868d1e21780b05b9bb2d5adc40bd9
benNjogu <kamandemachafi.gmail.com>
2022-03-03 04:32:03 +0300

    Filtering arrays. closes #5

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -1,19 +1,6 @@
 
-const numbers = [-1, -2, 3, -4];
+const numbers = [1, -2, 3, 4];
 
-const allPositive = numbers.every(function (value) {
-    return value >= 0;
-});
-/**
- * @The above method returns true if allPositive 
- * and false if any is negative.
- */
+const filtered = numbers.filter(value => value >= 0);
 
-const atLeastOnePositive = numbers.some(function (value) {
-    return value >= 0;
-});
-/**
- * @The above checks to see if at least one 
- * of the elements is positive in which it returns true
- */
-console.log(atLeastOnePositive);
+console.log(filtered);
