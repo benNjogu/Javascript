65d0579f93a2cdcb9601a276c9ea08cbcb86389b
benNjogu <kamandemachafi.gmail.com>
2022-03-03 04:48:30 +0300

    Returning an object filter. #5

--- Part6:Arrays/index.js
+++ Part6:Arrays/index.js
@@ -3,8 +3,10 @@ const numbers = [1, -2, 3, 4];
 
 const filtered = numbers.filter(value => value >= 0);
 
-const items = filtered.map(n => '<li>' + n + '</li>')
+const items = filtered.map(n => ({ value: n }));
+/**
+ * @Use brackets () around an object when 
+ * returning it using the arrow function.
+ */
 
-const html = '<ul>' + items.join('') + '</ul>';
-
-console.log(html);
+console.log(items);
