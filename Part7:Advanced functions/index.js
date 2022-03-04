20d86a72c10c86fe8db24bdc17d15b7fdcee6ad0
benNjogu <kamandemachafi@gmail.com>
2022-03-04 23:08:32 +0300

    Default parameters soln 2

--- Part7:Advanced functions/index.js
+++ Part7:Advanced functions/index.js
@@ -1,10 +1,7 @@
 
-function interest(principal, rate, years) {
-
-    rate = rate || 3.5;
-    years = years || 5;
+function interest(principal, rate = 3.5, years = 5) {
 
     return principal * rate / 100 * years;
 }
 
-console.log(interest(10000, 4));
+console.log(interest(10000));
