2285d03f9f0eb862cf090cdfbd6f9f22f1cd75c4
benNjogu <kamandemachafi@gmail.com>
2022-03-04 22:24:46 +0300

    Function decration and expressions

--- Part7:Advanced functions/index.js
+++ Part7:Advanced functions/index.js
@@ -1,2 +1,20 @@
 
+//Function declaration
+function walk() {
+    console.log('walk');
+}
+
+//Anonymous Function Expression
+let run = function () {
+    console.log('run');
+};
+
+let move = run;
+run();
+move();
+
+//Named Function Expression
+let running = function running() {
+    console.log('run');
+};
 
