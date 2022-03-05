c2b7c3062df1452b3c2336b44569c20ea541fefe
benNjogu <kamandemachafi@gmail.com>
2022-03-05 22:46:42 +0300

    Try and catch

--- Part7:Advanced functions/index.js
+++ Part7:Advanced functions/index.js
@@ -8,15 +8,24 @@ const person = {
     },
 
     set fullName(value) {
-        if (typeof value !== 'string') return;
+        if (typeof value !== 'string')
+            throw new Error('value is not a string.');
 
         const parts = value.split(' ');
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name.')
+
         this.firstName = parts[0];
         this.secondName = parts[1];
     }
 };
 
-person.fullName = null;
+try {
+    person.fullName = '';
+} catch (e) {
+    console.log(e);
+    alert(e);
+}
 
 //getters => access properties in an object
 //setters => change (mutate) then
