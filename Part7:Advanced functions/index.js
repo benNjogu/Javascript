e572d847367373beaa96ebfe292197908e762a44
benNjogu <kamandemachafi@gmail.com>
2022-03-05 22:17:53 +0300

    before getters and setters

--- Part7:Advanced functions/index.js
+++ Part7:Advanced functions/index.js
@@ -1,7 +1,7 @@
 
-function interest(principal, rate = 3.5, years = 5) {
-
-    return principal * rate / 100 * years;
+const person = {
+    firstName: 'ben',
+    secondName: 'Njogu'
 }
 
-console.log(interest(10000));
+console.log(`${person.firstName} ${person.secondName}`);
