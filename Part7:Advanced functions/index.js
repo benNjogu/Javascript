29e251f3b1b7963caa9baa7fbb39309359980f22
benNjogu <kamandemachafi@gmail.com>
2022-03-05 22:29:01 +0300

    Getters and Setters

--- Part7:Advanced functions/index.js
+++ Part7:Advanced functions/index.js
@@ -3,9 +3,19 @@ const person = {
     firstName: 'ben',
     secondName: 'Njogu',
 
-    fullName() {
+    get fullName() {
         return `${person.firstName} ${person.secondName}`
+    },
+
+    set fullName(value) {
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.secondName = parts[1];
     }
 };
 
-console.log(person.fullName());
+person.fullName = 'Peter wathiringi';
+
+//getters => access properties in an object
+//setters => change (mutate) then
+console.log(person.fullName);
