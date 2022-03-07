f788fa7486f93e18075a663e0fed26798121dc8a
benNjogu <kamandemachafi@gmail.com>
2022-03-07 04:02:42 +0300

    sum of arguments using rest operators.

--- Part7.1:Challenges/index.js
+++ Part7.1:Challenges/index.js
@@ -1,1 +1,12 @@
 
+/**
+ * Challenge1 = sum of arguments
+ * Write a function that takes arguments and returns their sum
+ */
+
+function sum(...items) {
+    return items.reduce((a, b) => a + b);
+}
+
+console.log(sum(1, 2, 3, 4));
+
