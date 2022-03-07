857c43c7e2387dad3411d34b5ecbace3c8c6e0e3
benNjogu <kamandemachafi@gmail.com>
2022-03-07 05:12:13 +0300

    The 4 pillars of OOP.

--- Part8:OOP/index.js
+++ Part8:OOP/index.js
@@ -4,12 +4,15 @@
  * 1.Encapsulation
  * -Group related variables and functions together
  * -Reduce complexity and increase reusability
+ * 
  * 2.Abstraction
  * -hide data
  * -makes interface simpler
  * -reduce complexity and reduce impact of change
+ * 
  * 3.Inheritance
  * -mechanism to eliminate redundant code
+ * 
  * 4.Polymorphism
  * -Many forms
  * -Refactor ugly switch/case statements
