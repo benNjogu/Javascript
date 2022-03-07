6f8bdadc019a1a63ba5dfb3da1daa94c952e867a
benNjogu <kamandemachafi@gmail.com>
2022-03-07 04:37:16 +0300

    Challenge3: error handling.

--- Part7.1:Challenges/index.js
+++ Part7.1:Challenges/index.js
@@ -1,17 +1,24 @@
 
 /**
- * Challenge2 = area of a circle
- * write a circle object where properties can be added 
- * but area is read only.
+ * Challenge3 = error handling
  * */
 
-let circle = {
-    radius: 1,
+try {
+    const numbers = [1, 2, 1, 4];
 
-    get area() {
-        return Math.PI * this.radius * this.radius;
-    }
+    const count = countOccurrences(true, 1);
+    console.log(count);
+
+} catch (error) {
+    console.log(error.message);
 }
 
-circle.radius = 7;
-console.log(circle.area);
+function countOccurrences(array, searchElement) {
+    if (!Array.isArray(array))
+        throw new Error('Invalid array.');
+
+    return array.reduce((accumulator, current) => {
+        const occurrence = (current === searchElement) ? 1 : 0;
+        return accumulator + occurrence;
+    }, 0);
+}
