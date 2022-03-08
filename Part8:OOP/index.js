28cc4b41d6e3561f4d54ae7fd759ee5507b42daa
benNjogu <kamandemachafi@gmail.com>
2022-03-08 04:48:40 +0300

    Constructor property

--- Part8:OOP/index.js
+++ Part8:OOP/index.js
@@ -1,32 +1,9 @@
 
-//factory function
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function () {
-            console.log('draw');
-        }
-    };
+let x = {};
 
-}
-
-const circle = createCircle(1);
-//circle.draw();
-
-//constructor function
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    };
-}
-
-const another = new Circle(1);
 /**
- * with the new operator above, 
- * 1. A new object is created
- * 2. It sets this to point to that new object
- * 3. It will return that object from the function
- * NB we dont have a return statement in the function
+ * The above will be translated to
+ * let x = new Object() by js engine.
  */
 
+//Every object has a constructor property which references the property which was use to create that object
