3c2f1de67e38e53d8a800cf81a8dc8fad9b4cb8e
benNjogu <kamandemachafi@gmail.com>
2022-03-08 04:41:58 +0300

    Constructor functions

--- Part8:OOP/index.js
+++ Part8:OOP/index.js
@@ -11,4 +11,22 @@ function createCircle(radius) {
 }
 
 const circle = createCircle(1);
-circle.draw();
+//circle.draw();
+
+//constructor function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    };
+}
+
+const another = new Circle(1);
+/**
+ * with the new operator above, 
+ * 1. A new object is created
+ * 2. It sets this to point to that new object
+ * 3. It will return that object from the function
+ * NB we dont have a return statement in the function
+ */
+
