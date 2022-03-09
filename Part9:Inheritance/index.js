20ddea59ac8e80cbe1ea93f699dc787db48bec65
benNjogu <kamandemachafi@gmail.com>
2022-03-09 10:49:34 +0300

    enumerating properties

--- Part9:Inheritance/index.js
+++ Part9:Inheritance/index.js
@@ -1,5 +1,5 @@
-/**
- * Objects created by the same constructor will have the same
- * prototype
- * 
- * */
+
+let person = { name: 'Ben' };
+console.log(person);
+
+console.log(Object.keys(person));//[ "name" ]
