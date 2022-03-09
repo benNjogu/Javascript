a56c7fb6c5c951ac3c8f9efeda090d6cbb6ec86b
benNjogu <kamandemachafi@gmail.com>
2022-03-09 10:15:56 +0300

    Inheritance theory

---
+++ Part9:Inheritance/index.js
@@ -0,0 +1,12 @@
+/**
+ * INHERITANCE:-
+ * 
+ * if circle inherits from shape
+ * shape is the base/super/parent class
+ * circle is the derived/sub/child
+ * the relationship is called 'IS-A' i.e. circle is a shape.
+ * 
+ * major types of inheritance in js"
+ * -> classical
+ * -> prototypical
+ */
