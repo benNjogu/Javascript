c39a62ec2a03e61c0a8e7a49f52027482ec470fd
benNjogu <kamandemachafi@gmail.com>
2022-03-09 05:06:33 +0300

    properties without functions

--- Part8:OOP/index.js
+++ Part8:OOP/index.js
@@ -10,4 +10,5 @@ function Circle(radius) {
 const circle = new Circle(10);
 
 for (let key in circle)
-    console.log(key, circle[key]);
+    if (typeof circle[key] !== 'function')
+        console.log(key, circle[key]);
