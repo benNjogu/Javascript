4777a3e28120e81efeda65f51c15ed9144c59dc9
benNjogu <kamandemachafi@gmail.com>
2022-03-09 05:13:36 +0300

    Check existence of properties in an object

--- Part8:OOP/index.js
+++ Part8:OOP/index.js
@@ -9,6 +9,15 @@ function Circle(radius) {
 
 const circle = new Circle(10);
 
+//soln 1: getting all keys and objects
 for (let key in circle)
     if (typeof circle[key] !== 'function')
         console.log(key, circle[key]);
+
+
+//soln 1: getting all keys and objects
+console.log(Object.keys(circle));
+
+//Equivalent of contains in Java
+if ('radius' in circle)
+    console.log('Circle has a radius');
