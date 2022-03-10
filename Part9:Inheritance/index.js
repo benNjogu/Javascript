6fdc5c79b4762f1dea86a6c8cf444c448729ee64
benNjogu <kamandemachafi@gmail.com>
2022-03-10 19:59:01 +0300

    Dont modify objects you dont own.

--- Part9:Inheritance/index.js
+++ Part9:Inheritance/index.js
@@ -1,23 +1,12 @@
 
-function Circle(radius) {
-    //instance members
-    this.radius = radius;
+Array.prototype.shuffle = function () {
 
-    this.move = function () {
-        console.log('move');
-    }
-}
+};
 
-//Prototype members
-Circle.prototype.draw = function () {
-    this.move();
-    console.log('draw');
-}
+const array = [];
+array.shuffle();
 
-const c1 = new Circle(1);
+console.log(array);
 
-//Only returns instant members
-console.log(Object.keys(c1));
-
-//Returns all members(instant + prototype)
-for (let key in c1) console.log(key);
+//Lesson
+//Dont modify objects you dont own.
