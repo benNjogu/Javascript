cd1eb4db35323102be8fb1ee39b602b9f33f4305
benNjogu <kamandemachafi@gmail.com>
2022-03-10 18:11:10 +0300

    Prototype vs instance members.

--- Part9:Inheritance/index.js
+++ Part9:Inheritance/index.js
@@ -1,6 +1,25 @@
 
 function Circle(radius) {
+    //instance members
     this.radius = radius;
+
+    this.move = function () {
+        console.log('move');
+    }
 }
 
-const circle = new Circle(1);
+//Prototype members
+//This writes the draw method on the prototype
+Circle.prototype.draw = function () {
+    this.move();
+    console.log('draw');
+}
+
+const c1 = new Circle(1);
+const c2 = new Circle(2);
+
+console.log(c1);
+
+Circle.prototype.toString = function () {
+    return 'Circle with radius ' + this.radius;
+}
