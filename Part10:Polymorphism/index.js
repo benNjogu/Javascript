7a6a92a4a226fedb012fcdb2d667c53158fcd80d
benNjogu <kamandemachafi@gmail.com>
2022-03-11 13:50:58 +0300

    Challenge 2. Polymorphism

--- Part10:Polymorphism/index.js
+++ Part10:Polymorphism/index.js
@@ -19,7 +19,27 @@ function HTMLSelectElement(items = []) {
     this.removeItem = function (item) {
         this.items.splice(this.items.indexOf(item), 1);
     }
+
+    this.render = function () {
+        return `
+        <select>${this.items.map(item => `
+            <option>${item}</option>`).join('')}
+        </select>`;
+    }
 }
 
 HTMLSelectElement.prototype = new HtmlElement();
 HTMLSelectElement.prototype.constructor = HTMLSelectElement;
+
+
+function HtmlImageElement(src) {
+    this.src = src;
+
+
+    this.render = function () {
+        return `<img src = "${this.src}" />`
+    }
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
