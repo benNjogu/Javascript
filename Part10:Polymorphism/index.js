9c0f4943abae686b2f1c5a0503cd84275e02af86
benNjogu <kamandemachafi@gmail.com>
2022-03-11 04:43:29 +0300

    Prototypical inheritance.

--- Part10:Polymorphism/index.js
+++ Part10:Polymorphism/index.js
@@ -1,2 +1,21 @@
 
+function Shape() {
 
+}
+Shape.prototype.duplicate = function () {
+    console.log('duplicate');
+}
+
+function Circle(radius) {
+    this.radius = radius;
+}
+
+//creates the prototypical inheritance
+Circle.prototype = Object.create(Shape.prototype);
+
+Circle.prototype.draw = function () {
+    console.log('draw');
+}
+
+const s = new Shape();
+const c = new Circle(1);
