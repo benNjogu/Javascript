e9787305c46e6a338bdd9793bdcb669318e9ed49
benNjogu <kamandemachafi@gmail.com>
2022-03-12 14:38:31 +0300

    Hoisting part 1

--- Part11:ES6Classes/index.js
+++ Part11:ES6Classes/index.js
@@ -1,6 +1,15 @@
 
+sayHello();//can be called from here. its hoisting!!
+
+sayGoodBye()//throws an error. the function is not hoisted!!
+
 //Function Declaration
 function sayHello() { }
 
 //Function Expression
 const sayGoodBye = function () { };
+
+/**
+ * NB: Function expressions are not hoisted.
+ * same as regular variables e.g. let number = 1
+ */
