88bc10db4b4daa36889e95d31dc88bc01e9f15c1
benNjogu <kamandemachafi@gmail.com>
2022-03-12 16:03:40 +0300

    Private methods using symbols.

--- Part11:ES6Classes/index.js
+++ Part11:ES6Classes/index.js
@@ -1,12 +1,18 @@
 
 
 const _radius = Symbol();
+const _draw = Symbol();
 class Circle {
     constructor(radius) {
         //this.radius = radius;
         //this['radius'] = radius;
         /**The above lines are the same */
         this[_radius] = radius;
+    }
+
+    //computed property names
+    [_draw]() {
+
     }
 }
 
