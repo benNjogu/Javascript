f9806d3a8a48ba3d6a2ad2c38ffd4362b6fdddf1
benNjogu <kamandemachafi@gmail.com>
2022-03-13 06:02:38 +0300

    Stack: tutors soln

--- Part11.1:Challenges/index.js
+++ Part11.1:Challenges/index.js
@@ -1,53 +1,42 @@
 
+const _items = new WeakMap();
 class Stack {
     constructor() {
-        this.count = 0;
-        this.item = null
-        this.top = 0;
-        this.itemsArray = [];
+        _items.set(this, []);
     }
 
     push(item) {
-        if (item == null) {
-            console.error('invalid item');
-            return;
-        }
+        if (item == null)
+            throw new Error('invalid item');
 
-        this.itemsArray.push(item);
-        this.count++;
-        this.top++;
+        _items.get(this).push(item);
     }
 
     get peek() {
-        if (this.itemsArray.length == 0) {
-            console.error('no items found');
-            return;
-        }
-        return this.itemsArray[--this.top];
+        const items = _items.get(this);
+
+        if (items.length === 0)
+            throw new Error('stack is empty.');
+
+        return items[items.length - 1];
+
     }
 
     pop() {
-        if (this.itemsArray.length == 0) {
-            console.error('stack is empty');
-            return;
-        }
+        const items = _items.get(this);
 
-        this.itemsArray.pop();
-        this.count--;
-        this.top--;
+        if (items.length === 0)
+            throw new Error('stack is empty.');
+
+        return items.pop();
     }
 
+    get count() {
+        return _items.get(this).length;
+    }
 }
 
 const stack = new Stack();
 stack.push(1);
 stack.push(2);
 stack.push(1);
-console.log(stack.count);
-console.log(stack.top);
-console.log(stack.peek);
-stack.pop
-stack.pop
-stack.pop
-stack.pop
-console.log(stack.count);
