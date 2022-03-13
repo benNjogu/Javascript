6f880bf4a5a8ddfac343dee674f8bbc670426779
benNjogu <kamandemachafi@gmail.com>
2022-03-13 13:01:04 +0300

    Before introducing modules.

--- Part12:Modules/index.js
+++ Part12:Modules/index.js
@@ -1,6 +1,15 @@
-/**
- * MODULARITY
- * a) Maintainability
- * b) Reusability
- * c) Abstraction
- */
+
+const _radius = new WeakMap();
+
+class Circle {
+    constructor(radius) {
+        _radius.set(this, radius);
+    }
+
+    draw() {
+        console.log('Circle with radius ' + _radius.get(this));
+    }
+}
+
+const c = new Circle(10);
+c.draw();
