76da6ca8a832f5090386eb3293dcbb35f827e684
benNjogu <kamandemachafi@gmail.com>
2022-03-13 13:19:03 +0300

    ES6 Tooling.

--- Part12:Modules/index.js
+++ Part12:Modules/index.js
@@ -2,3 +2,15 @@ import { Circle } from "./circle.js";
 
 const c = new Circle(10);
 c.draw();
+
+/**
+ * ES6 Tooling(browser tools)
+ * 1. Transpiler
+ * -Combination of two words, Translator and Compiler.
+ * -Converts modern js code to a code that all our browsers understand.
+ * -i.e. Modern -> ES5
+ * -e.g. BABEL
+ * 2. Bundler
+ * -Responsible for combining all js files to a single file called a bundle.
+ * -e.g. WEBPACK.
+ */
