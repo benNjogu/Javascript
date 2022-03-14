f3bb9bc6de6c5a75f8c393be7ee00f1264ad9934
benNjogu <kamandemachafi@gmail.com>
2022-03-14 13:25:01 +0300

    paths part 1

--- Canvas/index.js
+++ Canvas/index.js
@@ -7,3 +7,11 @@ for (let y = 10; y <= 100; y += 10) {
 }
 
 cx.stroke();
+
+/**
+ * This example creates a path with a number of horizontal line segments and
+   then strokes it using the stroke method. Each segment created with lineTo
+   starts at the path’s current position.
+   That position is usually the end of the last segment, unless moveTo was called. In that case, the next segment would
+   start at the position passed to moveTo .
+ */
