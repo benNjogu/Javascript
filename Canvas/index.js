d08c0200dcc2a25575b07957534e006dbcddd08d
benNjogu <kamandemachafi@gmail.com>
2022-03-14 14:32:12 +0300

    Storing and clearing transformations

--- Canvas/index.js
+++ Canvas/index.js
@@ -1,6 +1,14 @@
 let cx = document.querySelector("canvas").getContext("2d");
-cx.scale(3, .5);
-cx.beginPath();
-cx.arc(50, 50, 40, 0, 7);
-cx.lineWidth = 3;
-cx.stroke();
+function branch(length, angle, scale) {
+    cx.fillRect(0, 0, 1, length);
+    if (length < 8) return;
+    cx.save();
+    cx.translate(0, length);
+    cx.rotate(-angle);
+    branch(length * scale, angle, scale);
+    cx.rotate(2 * angle);
+    branch(length * scale, angle, scale);
+    cx.restore();
+}
+cx.translate(300, 0);
+branch(60, 0.5, 0.8);
