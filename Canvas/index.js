a463c269831ea08073f172cf8b926e11834c248d
benNjogu <kamandemachafi@gmail.com>
2022-03-14 13:04:51 +0300

    Lines and surfaces.

--- Canvas/index.js
+++ Canvas/index.js
@@ -1,13 +1,10 @@
-let canvas = document.querySelector("canvas");
-let context = canvas.getContext("2d");
-context.fillStyle = "red";
-context.fillRect(10, 10, 100, 50);
+let cx = document.querySelector("canvas").getContext("2d");
+cx.strokeStyle = "blue";
+cx.strokeRect(5, 5, 50, 50);
+cx.lineWidth = 5;
+cx.strokeRect(135, 5, 50, 50);
 
 /**
- * After creating the context object, the example draws a red rectangle 100
- * pixels wide and 50 pixels high, with its top-left corner at coordinates (10,10).
+ * This code draws two blue squares, using a thicker line for the second one.
  * 
- * Just like in HTML (and SVG), the coordinate system that the canvas uses
- * puts (0,0) at the top-left corner, and the positive y-axis goes down from there.
- * So (10,10) is 10 pixels below and to the right of the top-left corner.
  */
