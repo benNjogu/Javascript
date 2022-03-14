a5a19baf880d008511fef655e332e569fd2ba5ed
benNjogu <kamandemachafi@gmail.com>
2022-03-14 14:08:18 +0300

    Pie Chart

--- Canvas/index.js
+++ Canvas/index.js
@@ -1,7 +1,31 @@
+/*Imagine you’ve just taken a job at EconomiCorp, Inc., and your first assignment
+is to draw a pie chart of its customer satisfaction survey results.
+The results binding contains an array of objects that represent the survey
+responses.*/
+
+const results = [
+    { name: "Satisfied", count: 1043, color: "lightblue" },
+    { name: "Neutral", count: 563, color: "lightgreen" },
+    { name: "Unsatisfied", count: 510, color: "pink" },
+    { name: "No comment", count: 175, color: "silver" }
+];
+
 let cx = document.querySelector('canvas').getContext('2d');
-cx.beginPath();
-// center=(50,50) radius=40 angle=0 to 7
-cx.arc(50, 50, 40, 0, 7);
-// center=(150,50) radius=40 angle=0 to π ½
-cx.arc(150, 50, 40, 0, 0.5 * Math.PI);
-cx.stroke();
+let total = results
+    .reduce((sum, { count }) => sum + count, 0);
+
+//start at the top
+let currentAngle = -0.5 * Math.PI;
+for (let result of results) {
+    let sliceAngle = (result.count / total) * 2 * Math.PI;
+    cx.beginPath();
+    // center=100,100, radius=100
+    // from current angle, clockwise by slice's angle
+    cx.arc(100, 100, 100,
+        currentAngle, currentAngle + sliceAngle);
+    currentAngle += sliceAngle;
+    cx.lineTo(100, 100);
+    cx.fillStyle = result.color;
+    cx.fill();
+}
+
