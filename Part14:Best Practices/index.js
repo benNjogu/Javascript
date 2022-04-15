4654aedb84ab056276f105a62245a6c9baf057c6
benNjogu <kamandemachafi@gmail.com>
2022-04-15 04:25:57 +0300

    The deep nesting problem

--- Part14:Best Practices/index.js
+++ Part14:Best Practices/index.js
@@ -11,3 +11,20 @@ const SECONDS_IN_A_DAY = 86400;
 for (let i = 0; i < SECONDS_IN_A_DAY; i++) {
 
 }
+
+/**
+ * Deep Nesting -> If your code contains many nested loops or conditional statements, 
+ * this should be extracted into a separate function
+ * In example below, we have broken rules e.g. deep nesting and code repetition.
+ * 
+ * 
+ */
+
+const exampleArray = [[['value']]];
+exampleArray.forEach((array1) => {
+    exampleArray.forEach((array2) => {
+        exampleArray.forEach((element) => {
+            console.log(element);
+        })
+    })
+})
