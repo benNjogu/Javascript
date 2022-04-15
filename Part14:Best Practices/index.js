1f1fa0d36233bf3f1a8b4d6f75606306567809d1
benNjogu <kamandemachafi@gmail.com>
2022-04-15 05:03:32 +0300

    Deep nesting soln

--- Part14:Best Practices/index.js
+++ Part14:Best Practices/index.js
@@ -17,14 +17,17 @@ for (let i = 0; i < SECONDS_IN_A_DAY; i++) {
  * this should be extracted into a separate function
  * In example below, we have broken rules e.g. deep nesting and code repetition.
  * 
- * 
+ * The solution is extracting the loops in a separate function.
  */
 
 const exampleArray = [[['value']]];
-exampleArray.forEach((array1) => {
-    exampleArray.forEach((array2) => {
-        exampleArray.forEach((element) => {
-            console.log(element);
-        })
-    })
-})
+
+const retrieveFinalValue = (element) => {
+    if (Array.isArray(element)) {
+        return retrieveFinalValue(element[0])
+    }
+
+    return element;
+}
+
+retrieveFinalValue(exampleArray);
